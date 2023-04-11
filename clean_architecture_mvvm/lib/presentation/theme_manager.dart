9705831e6ea3a356d538795a50c0f95229ec8bb9
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-04-11 13:48:05 +0700

    adding_button

--- clean_architecture_mvvm/lib/presentation/theme_manager.dart
+++ clean_architecture_mvvm/lib/presentation/theme_manager.dart
@@ -30,10 +30,28 @@ ThemeData getApplicationTheme() {
       color: ColorManager.primary,
       elevation: AppSize.s4,
       shadowColor: ColorManager.primaryOpacity70,
-      titleTextStyle: getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+      titleTextStyle:
+          getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
     ),
     //button theme
-
+    buttonTheme: ButtonThemeData(
+      shape: StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.primaryOpacity70,
+    ),
+//elevated button themee
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(
+          color: ColorManager.white,
+        ),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
     //Text theme
 
     //input decoration theme(Text form)
