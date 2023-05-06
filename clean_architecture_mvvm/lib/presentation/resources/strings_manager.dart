181d0160a502e5c149dcd16967607379ee68cffb
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-05-06 15:55:52 +0700

    assets

--- clean_architecture_mvvm/lib/presentation/resources/strings_manager.dart
+++ clean_architecture_mvvm/lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,14 @@
 class AppStrings{
   static const String noRouteFound = "No Route Found";
+  static const String onBoardingTitle1 = "SEE THE BEST PERSON #1";
+  static const String onBoardingTitle2 = "SEE THE BEST PERSON #2";
+  static const String onBoardingTitle3 = "SEE THE BEST PERSON #3";
+  static const String onBoardingTitle4 = "SEE THE BEST PERSON #4";
+
+  static const String onBoardingSubTitle1 = "Tut app is an awesome flutter application using clean architecture #1";
+  static const String onBoardingSubTitle2 = "Tut app is an awesome flutter application using clean architecture #2";
+  static const String onBoardingSubTitle3 = "Tut app is an awesome flutter application using clean architecture #3";
+  static const String onBoardingSubTitle4 = "Tut app is an awesome flutter application using clean architecture #4";
+  
+
 }
