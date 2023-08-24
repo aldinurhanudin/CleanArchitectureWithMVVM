fb75e7bd4adbc91bd1d8c8a299bcc403e354bc68
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-08-24 15:45:46 +0700

    data_checker

--- clean_architecture_mvvm/lib/data/network/app_api.g.dart
+++ clean_architecture_mvvm/lib/data/network/app_api.g.dart
@@ -21,30 +21,34 @@ class _AppServiceClient implements AppServiceClient {
   String? baseUrl;
 
   @override
-  Future<AuthenticationResponse> login() async {
+  Future<AuthenticationResponse> login(
+    String email,
+    String password,
+    String imei,
+    String deviceType,
+  ) async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
-    final Map<String, dynamic>? _data = null;
-    // final _result = await _dio.fetch<Map<String, dynamic>>(
-    //     _setStreamType<AuthenticationResponse>(
-    //       Options(
-    //   method: 'POST',
-    //   headers: _headers,
-    //   extra: _extra,
-    // )
-    //         .compose(
-    //           _dio.options,
-    //           '/customers/login',
-    //           queryParameters: queryParameters,
-    //           data: _data,
-    //         )
-    //         .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final _result = await _dio.fetch<Map<String,dynamic>>(
-      _setStreamType<AuthenticationResponse>(
-        Options(method:'POST', headers: <String,dynamic>{}, extra: _extra).compose(_dio.options, '/customers/login', queryParameters: queryParameters,data: _data).copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)
-      )
-    );
+    final _data = {
+      'email': email,
+      'password': password,
+      'imei': imei,
+      'deviceType': deviceType,
+    };
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<AuthenticationResponse>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/customers/login',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
     final value = AuthenticationResponse.fromJson(_result.data!);
     return value;
   }
---
+++ clean_architecture_mvvm/lib/data/network/network_info.dart
@@ -0,0 +1,11 @@
+abstract class NetworkInfo{
+  Future<bool> get isConnected;
+
+}
+
+class NetworkInfoImpl implements NetworkInfo{
+  DataConnectionChecker _dataConnectionChecker;
+  NetworkInfoImpl(this._dataConnectionChecker);
+  @override
+  Future<bool> get isConnected => _dataConnectionChecker.hasConnection;
+}
