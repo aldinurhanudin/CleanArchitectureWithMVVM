0030bfbe390a09429cff7e66ef6be2be3a86003f
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-08-24 15:24:56 +0700

    data_source

---
+++ clean_architecture_mvvm/lib/data/data_source/remote_data_source.dart
@@ -0,0 +1,18 @@
+import 'package:clean_architecture_mvvm/data/network/app_api.dart';
+import 'package:clean_architecture_mvvm/data/request/request.dart';
+import 'package:clean_architecture_mvvm/data/responses/responses.dart';
+import 'package:clean_architecture_mvvm/domain/model.dart';
+
+abstract class RemoteDataSource {
+  Future<AuthenticationResponse>login(LoginRequest loginRequest);
+}
+
+class RemoteDataSourceImplenter implements RemoteDataSource{
+  AppServiceClient _appServiceClient;
+  RemoteDataSourceImplenter(this._appServiceClient);
+  @override
+  Future<AuthenticationResponse> login(LoginRequest loginRequest)async{
+    return await _appServiceClient.login(loginRequest.email,loginRequest.password, loginRequest.imei, loginRequest.deviceType);
+
+  }
+}
