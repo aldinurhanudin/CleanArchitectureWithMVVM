38c3359d0a945f80fe34f24248dd9ed45184cd51
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-09-04 14:36:51 +0700

    response

--- clean_architecture_mvvm/lib/data/network/error_handler.dart
+++ clean_architecture_mvvm/lib/data/network/error_handler.dart
@@ -8,8 +8,28 @@ enum DataSource{
   INTERNAL_SERVER_ERROR,
   CONNECT_TIMEOUT,
   CANCEL,
-  RECIEVE_TIMEOUT,
+  RECEIVE_TIMEOUT,
   SEND_TIMEOUT,
   CACHE_ERROR,
   NO_INTERNET_CONNECTION
+}
+
+class ResponseCode{
+  static const int SUCCESS=200;
+  static const int NO_CONTENT=201;
+  static const int BAD_REQUEST=400;
+  static const int FORBIDDEN=403;
+  static const int UNAUTHORISED=401;
+  static const int NOT_FOUND=404;
+  static const int INTERNAL_SERVER_ERROR=500;
+
+
+  static const int UNKNOWN=-1;
+  static const int CONNECT_TIMEOUT=-2;
+  static const int CANCEL=-3;
+  static const int RECEIVE_TIMEOUT=-4;
+  static const int SEND_TIMEOUT=-5;
+  static const int CACHE_ERROR=-6;
+  static const int NO_INTERNET_CONNECTION=-7;
+
 }
