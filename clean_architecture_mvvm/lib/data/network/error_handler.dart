a2de2cf0c47b906b0bb7f7ccc61226a2c7f9b3dd
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-09-04 15:28:26 +0700

    responses

--- clean_architecture_mvvm/lib/data/network/error_handler.dart
+++ clean_architecture_mvvm/lib/data/network/error_handler.dart
@@ -14,7 +14,7 @@ enum DataSource{
   NO_INTERNET_CONNECTION
 }
 
-class ResponseCode{
+class ResponseData{
   static const int SUCCESS=200;
   static const int NO_CONTENT=201;
   static const int BAD_REQUEST=400;
@@ -31,5 +31,25 @@ class ResponseCode{
   static const int SEND_TIMEOUT=-5;
   static const int CACHE_ERROR=-6;
   static const int NO_INTERNET_CONNECTION=-7;
+
+}
+
+class ResponseMessage{
+  static const String SUCCESS="success";
+  static const String NO_CONTENT="sucess with no content";
+  static const String BAD_REQUEST="Bad request, try again later";
+  static const String FORBIDDEN="forbiden request, try again later";
+  static const String UNAUTHORISED="user is unauthorised, try again later";
+  static const String NOT_FOUND="Url is not found, try again later";
+  static const String INTERNAL_SERVER_ERROR="some thing went wrong, try again later";
+
+
+  static const String UNKNOWN="some thing went wrong, try again later";
+  static const String CONNECT_TIMEOUT="time out error, try again later";
+  static const String CANCEL="request was cancelled, try again later";
+  static const String RECEIVE_TIMEOUT="time out error,try again later";
+  static const String SEND_TIMEOUT="time out error, try again later";
+  static const String CACHE_ERROR="Cache error, try again later";
+  static const String NO_INTERNET_CONNECTION="Please check your internet connection";
 
 }
