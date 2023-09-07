90af7d9cc621dd459535f75459a23034bfbc1dfd
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-09-07 15:04:16 +0700

    adding_logger

--- clean_architecture_mvvm/lib/data/network/dio_factory.dart
+++ clean_architecture_mvvm/lib/data/network/dio_factory.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../../app/constant.dart';
 const String APPLICATION_JSON  = "application/json";
@@ -22,6 +24,15 @@ Duration _timeOut = Duration(seconds: 60*1000);
       receiveTimeout: _timeOut,
       headers: headers
     );
+    if(kReleaseMode) {
+      print("release mode no logs");
+    }else{
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+      ));
+    }
     return dio;
   }
 }
--- clean_architecture_mvvm/lib/data/network/error_handler.dart
+++ clean_architecture_mvvm/lib/data/network/error_handler.dart
@@ -28,35 +28,69 @@ class ErrorHandler implements Exception {
       failure = DataSource.DEFAULT.getFailure();
     }
   }
-  Failure _handleError(DioError error) {
-    switch (error.type) {
-      case DioErrorType.connectionTimeout:
-        return DataSource.CONNECT_TIMEOUT.getFailure();
-      case DioErrorType.sendTimeout:
-        return DataSource.SEND_TIMEOUT.getFailure();
-      case DioErrorType.receiveTimeout:
-        return DataSource.RECEIVE_TIMEOUT.getFailure();
-      case DioErrorType.response:
-        switch(error.response?.statusCode){
-          case ResponseCode.BAD_REQUEST:
+  // Failure _handleError(DioError error) {
+  //   switch (error.type) {
+  //     case DioErrorType.connectionTimeout:
+  //       return DataSource.CONNECT_TIMEOUT.getFailure();
+  //     case DioErrorType.sendTimeout:
+  //       return DataSource.SEND_TIMEOUT.getFailure();
+  //     case DioErrorType.receiveTimeout:
+  //       return DataSource.RECEIVE_TIMEOUT.getFailure();
+  //     case DioErrorType.response:
+  //       switch(error.response?.statusCode){
+  //         case ResponseCode.BAD_REQUEST:
+  //         return DataSource.BAD_REQUEST.getFailure();
+  //         case ResponseCode.FORBIDDEN:
+  //         return DataSource.FORBIDDEN.getFailure();
+  //         case ResponseCode.UNAUTHORISED:
+  //         return DataSource.UNAUTHORISED.getFailure();
+  //         case ResponseCode.NOT_FOUND:
+  //         return DataSource.NOT_FOUND.getFailure();
+  //         case ResponseCode.INTERNAL_SERVER_ERROR:
+  //         return DataSource.INTERNAL_SERVER_ERROR.getFailure();
+  //         default:
+  //         return DataSource.DEFAULT.getFailure();
+  //       }
+  //     case DioErrorType.cancel:
+  //       return DataSource.CONNECT_TIMEOUT.getFailure();
+  //     case DioErrorType.other:
+  //       return DataSource.DEFAULT.getFailure();
+  //   }
+  // }
+
+Failure _handleError(DioError error) {
+  switch (error.type) {
+    case DioErrorType.connectionTimeout:
+      return DataSource.CONNECT_TIMEOUT.getFailure();
+    case DioErrorType.sendTimeout:
+      return DataSource.SEND_TIMEOUT.getFailure();
+    case DioErrorType.receiveTimeout:
+      return DataSource.RECEIVE_TIMEOUT.getFailure();
+    case DioErrorType.response:
+      switch(error.response?.statusCode){
+        case ResponseCode.BAD_REQUEST:
           return DataSource.BAD_REQUEST.getFailure();
-          case ResponseCode.FORBIDDEN:
+        case ResponseCode.FORBIDDEN:
           return DataSource.FORBIDDEN.getFailure();
-          case ResponseCode.UNAUTHORISED:
+        case ResponseCode.UNAUTHORISED:
           return DataSource.UNAUTHORISED.getFailure();
-          case ResponseCode.NOT_FOUND:
+        case ResponseCode.NOT_FOUND:
           return DataSource.NOT_FOUND.getFailure();
-          case ResponseCode.INTERNAL_SERVER_ERROR:
+        case ResponseCode.INTERNAL_SERVER_ERROR:
           return DataSource.INTERNAL_SERVER_ERROR.getFailure();
-          default:
+        default:
           return DataSource.DEFAULT.getFailure();
-        }
-      case DioErrorType.cancel:
-        return DataSource.CONNECT_TIMEOUT.getFailure();
-      case DioErrorType.other:
-        return DataSource.DEFAULT.getFailure();
-    }
+      }
+    case DioErrorType.cancel:
+      return DataSource.CONNECT_TIMEOUT.getFailure();
+    case DioErrorType.other:
+      return DataSource.DEFAULT.getFailure();
+    default:
+      throw Exception('Unhandled DioError type: ${error.type}');
   }
+}
+
+
 }
 
 extension DataSourceExtension on DataSource {
